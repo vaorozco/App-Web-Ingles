6f0537afcbd228a03c7854ff7715c90aceae37b0
Víctor <vaorozco@hotmail.com>
2020-10-12 23:32:55 -0600

    Conexión a bd y ejecución de sp registro

--- Proyecto/app/src/main/java/com/example/proyecto/ConnectionHelper.java
+++ Proyecto/app/src/main/java/com/example/proyecto/ConnectionHelper.java
@@ -3,47 +3,27 @@ package com.example.proyecto;
 import android.annotation.SuppressLint;
 import android.os.StrictMode;
 import android.util.Log;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
+
 
 public class ConnectionHelper {
     @SuppressLint("NewApi")
-    public static Connection CONN() {
-
-        String _user = "sa";
-        String _pass = "789";
-        String _DB = "Examen1Bases2";
-        String _server = "138.94.57.63//MSSQLSERVER01";//ip 138.94.57.63
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-                .permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        /*String jdbcDriver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-        String connectionUrl = "jdbc:sqlserver://localhost\\MSSQLSERVER01;database=proyectoBases;integratedSecurity=true;";*/
-        //Connection con = DriverManager.getConnection("jdbc:jtds:sqlserver://10.0.2.2:1433/androidtest","usernameMSSQL","passwordMSSQL");
-        Connection conn = null;
-        String ConnURL = null;
-        //try {
-            //Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");//*net.sourceforge.jtds.jdbc.Driver").newInstance();*/
-            //ConnURL = //"fonet.database.windows.net;database=proyecto_fonet;user=avalverder;password=c3rv@nt3s;;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-              //      "jdbc:sqlserver://127.0.0.1//MSSQLSERVER01;database=Examen1Bases2;integratedSecurity=true;";
-                    //"jdbc:jtds:sqlserver://127.0.0.1:1433;instanceName=MSSQLSERVER01;"+ "databaseName=Examen1Bases2;integratedSecurity=true;";
-            /*ConnURL = "jdbc:jtds:sqlserver://" + _server + ";"
-                    + "databaseName=" + _DB + ";user=" + _user + ";password="
-                    + _pass + ";";*/
-            //conn = DriverManager.getConnection(ConnURL);
-            //Statement m_Statement = conn.createStatement();
-            //String query = "Insert into Cliente (Nombre,ApellidoFamilia, Correo, Contraseña) values" +
-              //      "('Conexión', 'Android', 'androidsql@hotmail.com','siuu')";
-        /*} catch (SQLException se) {
+    public static Connection conexionBD() {
+        Connection conexion = null;
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+            String url = String.format("jdbc:jtds:sqlserver://fonet.database.windows.net:1433/proyecto_fonet;user=avalverder;password=c3rv@nt3s;encrypt=true;trustServerCertificate=false;hostNameInCertificate=fonet.database.windows.net;loginTimeout=30;");
+            conexion = DriverManager.getConnection(url);
+        } catch (SQLException se) {
             Log.e("ERRO", se.getMessage());
         } catch (ClassNotFoundException e) {
             Log.e("ERRO", e.getMessage());
         } catch (Exception e) {
             Log.e("ERRO", e.getMessage());
-        }*/
-        return conn;}
+        }
+        return conexion;}
     }
-//}
--- Proyecto/app/src/main/java/com/example/proyecto/Registro.java
+++ Proyecto/app/src/main/java/com/example/proyecto/Registro.java
@@ -2,23 +2,16 @@ package com.example.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
 import com.google.android.material.snackbar.Snackbar;
-
+import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+import static com.example.proyecto.ConnectionHelper.conexionBD;
 
 public class Registro extends AppCompatActivity {
     EditText nombre, apellido, correo, contrasena, contrasena2;
@@ -37,81 +30,9 @@ public class Registro extends AppCompatActivity {
         constraintLayout = findViewById(R.id.conRegistro);
         registrarme = findViewById(R.id.button5);
         this.setTitle("Registro"); //título del header
-
         registrarme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pruebaConexion();
-            }
-        });
-    }
-    public Connection conexionBD(){
-
-        Connection conexion=null;
-        try {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
-            //conexion=DriverManager.getConnection("jdbc:jtds:sqlserver://fonet.database.windows.net,1433;Initial Catalog=proyecto_fonet;Persist Security Info=False;User ID=avalverder;Password=c3rv@nt3s;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            //conexion=DriverManager.getConnection("jdbc:jtds:sqlserver://fonet.database.windows.net,1433;Initial Catalog=proyecto_fonet;Persist Security Info=False;User ID=avalverder;Password=c3rv@nt3s;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            String url = String.format("jdbc:jtds:sqlserver://fonet.database.windows.net:1433/proyecto_fonet;user=avalverder;password=c3rv@nt3s;encrypt=true;trustServerCertificate=false;hostNameInCertificate=fonet.database.windows.net;loginTimeout=30;");
-            conexion = DriverManager.getConnection(url);
-
-
-            //conexion=DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.0.110/Examen1Bases2;instance=VAOC-PC\\MSSQLSERVER01");
-        }catch (Exception e){
-            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
-        }
-        return conexion;
-    }
-
-    public void pruebaConexion(){
-        try {
-            Toast.makeText(getApplicationContext(),"Conectado",Toast.LENGTH_SHORT).show();
-            //jdbc:jtds:sqlserver://mymssqlserver.corp.company.com:1433/mydatabase;instance=myinstance;domain=corp.company.com;
-            //Statement statement = conexionBD().createStatement();
-            //String query = "Insert into Cliente (Nombre,ApellidoFamilia, Correo, Contraseña) values ('Conexión', 'Android', 'anddsql@hotmail.com','siuu')";
-            PreparedStatement pst = conexionBD().prepareStatement("Insert into Usuario (nombre,apellido, correo, contraseña) values (?,?,?,?)");
-            pst.setString(1,nombre.getText().toString());
-            pst.setString(2,apellido.getText().toString());
-            pst.setString(3,correo.getText().toString());
-            pst.setString(4,contrasena.getText().toString());
-            pst.executeUpdate();
-            //statement.executeQuery(query);
-            Toast.makeText(getApplicationContext(),"Registrado",Toast.LENGTH_SHORT).show();
-        }catch (SQLException e){
-            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
-        }
-    }
-
-}
-
-
-
-        //registrarme.setOnClickListener(new View.OnClickListener() {
-          //  @Override
-           // public void onClick(View v) {
-                //String jdbcDriver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-                //String connectionUrl = "jdbc:sqlserver://localhost//MSSQLSERVER01;database=Examen1Bases2;integratedSecurity=true;";
-                /*try {
-                    Class.forName(jdbcDriver);
-
-                } catch (ClassNotFoundException ex) {
-                    System.out.println("No funciona");
-                }
-                try (Connection con = DriverManager.getConnection(connectionUrl);) {
-                    System.out.println("Conectado");
-                    Statement m_Statement = con.createStatement();
-                    String query = "Insert into Cliente (Nombre,ApellidoFamilia, Correo, Contraseña) values" +
-                            "('Conexión', 'Android', 'anddsql@hotmail.com','siuu')";
-                    m_Statement.executeQuery(query);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-}
 
                 if (isEmpty(nombre.getText().toString()) || isEmpty(apellido.getText().toString()) ||
                         isEmpty(correo.getText().toString()) || isEmpty(contrasena.getText().toString()) ||
@@ -120,8 +41,8 @@ public class Registro extends AppCompatActivity {
                 else if (!contrasena.getText().toString().equals(contrasena2.getText().toString()))
                     ShowSnackBar("Las contraseñas no coinciden");
                 else {
-                    AddUsers addUsers = new AddUsers();
-                    addUsers.execute("");*//*
+                    agregarUsuario agregarUsuario = new agregarUsuario();
+                    agregarUsuario.execute("");
                 }
             }
         });
@@ -132,11 +53,7 @@ public class Registro extends AppCompatActivity {
                 .setAction("CERRAR", new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-
-                    }
-                })
-                .setActionTextColor(getResources().getColor(android.R.color.holo_red_light))
-                .show();
+                    }}).setActionTextColor(getResources().getColor(android.R.color.holo_red_light)).show();
     }
 
     public Boolean isEmpty(String strValue) {
@@ -146,10 +63,8 @@ public class Registro extends AppCompatActivity {
             return false;
     }
 
-    private class AddUsers extends AsyncTask<String, Void, String> {
+    private class agregarUsuario extends AsyncTask<String, Void, String> {
         String Nombre, Apellido, Correo, Contrasena, Contrasena2; // deben ser diferentes a las primeras, estas inician con mayúscula
-
-
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -164,25 +79,19 @@ public class Registro extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... params) {
-
             try {
                 ConnectionHelper con = new ConnectionHelper();
-                Connection connect = ConnectionHelper.CONN();
-
-                /*String queryStmt = "Insert into Cliente (Nombre,ApellidoFamilia, Correo, Contraseña) values" +
-                        "('Conexión', 'Android', 'anddsql@hotmail.com','siuu22')";//"Insert into Cliente " + "(Nombre, ApellidoFamilia, Correo, Contraseña) values " +
-                        //"("+ Nombre + "," + Apellido + ","+ Correo + "," + Contrasena+ ")";
-
-                PreparedStatement preparedStatement = connect.prepareStatement(queryStmt);
-
-                preparedStatement.executeUpdate();
-
-                preparedStatement.close();*//*
-                Statement m_Statement = connect.createStatement();
-                String query = "Insert into Cliente (Nombre,ApellidoFamilia, Correo, Contraseña) values" +
-                        "('Conexión', 'Android', 'anddsql@hotmail.com','siuu2')";
-                m_Statement.executeQuery(query);
-
+                Connection connect = conexionBD();
+                String storedProcudureCall = "{call InsertarUsuario(?,?,?,?,?)};"; //stored procedure a llamar
+                CallableStatement sp = conexionBD().prepareCall(storedProcudureCall); //formato para llamar stored procedures en la base
+                sp.setString(1,nombre.getText().toString());
+                sp.setString(2,apellido.getText().toString());
+                sp.setString(3,correo.getText().toString());
+                sp.setString(4,contrasena.getText().toString());
+                sp.setInt(5,2);//(5,correo.getText().toString()); número 2 es tipo estudiante
+                sp.executeUpdate();
+                sp.close();
+                conexionBD().close();
                 return "¡Ha sido registrado!";
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -194,11 +103,12 @@ public class Registro extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String result) {
-
-            //Toast.makeText(signup.this, result, Toast.LENGTH_SHORT).show();
             ShowSnackBar(result);
             //progressBar.setVisibility(View.GONE);
             registrarme.setVisibility(View.VISIBLE);
             if (result.equals("Added successfully")) {
                 // Clear();
-            }*/
+            }
+        }
+    }
+}
