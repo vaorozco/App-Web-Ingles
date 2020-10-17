36ce844487d9ff28b5709737939a0663da61519d
Víctor <vaorozco@hotmail.com>
2020-10-17 10:36:26 -0600

    Menú desplegable

--- Proyecto/app/src/main/java/com/example/proyecto/MenuPrincipal.java
+++ Proyecto/app/src/main/java/com/example/proyecto/MenuPrincipal.java
@@ -1,21 +1,44 @@
 package com.example.proyecto;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MenuPrincipal extends AppCompatActivity implements View.OnClickListener{
+import com.google.android.material.navigation.NavigationView;
+
+public class MenuPrincipal extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
     ImageView imagenJuegos;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar1);
+        /*-------------------------------------Toolbar-------------------------------------*/
+        setSupportActionBar(toolbar);
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toogle);
+        toogle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
         imagenJuegos = (ImageView) findViewById(R.id.imageView5);
         //botonRegistro = (Button) findViewById(R.id.button3);
         //textoRecuperar = (TextView) findViewById(R.id.textView2);
@@ -23,6 +46,35 @@ public class MenuPrincipal extends AppCompatActivity implements View.OnClickList
         //botonRegistro.setOnClickListener(this);
         //textoRecuperar.setOnClickListener(this);
     }
+
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem){
+
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                break;
+            case R.id.nav_profile:
+                Intent intent = new Intent(MenuPrincipal.this,MiPerfil.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_exit:
+                Intent intent2 = new Intent(MenuPrincipal.this,Login.class);
+                startActivity(intent2);
+        }
+    drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
     /*@Override
     public void onClick(View v) {
         abrirRecuperarContrasena();
@@ -46,4 +98,5 @@ public class MenuPrincipal extends AppCompatActivity implements View.OnClickList
             startActivity(in);
         }*/
     }
+
 }
--- Proyecto/app/src/main/java/com/example/proyecto/RecuperarContrasena.java
+++ Proyecto/app/src/main/java/com/example/proyecto/RecuperarContrasena.java
@@ -2,7 +2,11 @@ package com.example.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class RecuperarContrasena extends AppCompatActivity {
 
@@ -11,4 +15,29 @@ public class RecuperarContrasena extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recuperar_contrasena);
     }
+
+    /*//Log.i("Send email", "");
+
+    String[] TO = {"vaorozco@hotmail.com"};
+    //String[] CC = {"mcmohd@gmail.com"};
+    Intent emailIntent = new Intent(Intent.ACTION_SEND);
+    emailIntent.setData(Uri.parse("mailto:"));
+    emailIntent.setType("text/plain");
+
+
+      emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+      emailIntent.putExtra(Intent.EXTRA_CC, CC);
+      emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+      emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+      try {
+        startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+        finish();
+        Log.i("Finished sending email.", "");
+    } catch(android.content.ActivityNotFoundException ex) {
+        Toast.makeText(RecuperarContrasena.this,
+                "There is no email client installed.", Toast.LENGTH_SHORT).show();
+    }
+*/
+
 }
